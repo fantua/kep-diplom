7c3ffc36b42b41d08f5e9da61956f82a7162a3a5
Igor <ihor3000@gmail.com>
2014-05-10 01:47:51 +0300

    rooms model

---
+++ app/models/Classrooms.php
@@ -0,0 +1,48 @@
+<?php
+
+
+
+
+class Classrooms extends \Phalcon\Mvc\Model
+{
+
+    /**
+     *
+     * @var integer
+     */
+    public $id;
+     
+    /**
+     *
+     * @var string
+     */
+    public $name;
+     
+    /**
+     * @return Classrooms[]
+     */
+    public static function find($parameters = array())
+    {
+        return parent::find($parameters);
+    }
+
+    /**
+     * @return Classrooms
+     */
+    public static function findFirst($parameters = array())
+    {
+        return parent::findFirst($parameters);
+    }
+
+    /**
+     * Independent Column Mapping.
+     */
+    public function columnMap()
+    {
+        return array(
+            'id' => 'id', 
+            'name' => 'name'
+        );
+    }
+
+}
